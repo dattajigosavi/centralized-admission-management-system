e7b4899a5e8857e51aa2d378fc60a8d480c45383
dattajigosavi <42787984+dattajigosavi@users.noreply.github.com>
2026-01-03 20:15:56 +0530

    Update app.js
    Add first API to get students list

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,35 @@
 const express = require("express");
 const app = express();
 
+// Temporary student data (like Google Sheets)
+const students = [
+    {
+        student_id: 1,
+        name: "Rahul Patil",
+        mobile: "9876543210",
+        preferred_branch: "B.Tech Engineering",
+        assigned_unit: "Engineering",
+        status: "New"
+    },
+    {
+        student_id: 2,
+        name: "Sneha Joshi",
+        mobile: "9123456789",
+        preferred_branch: "LLB",
+        assigned_unit: "Law",
+        status: "Called"
+    }
+];
+
+
 // This route runs when someone opens the root URL
 app.get("/", (req, res) => {
     res.send("Admission Management Backend is running");
+});
+
+// API to get all students
+app.get("/students", (req, res) => {
+    res.json(students);
 });
 
 // Start the server
